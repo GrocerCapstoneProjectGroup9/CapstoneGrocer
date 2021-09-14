3a9ce3c957dec75749514c71fd46df654f9b047a
mkhan14 <mkhan6241@bths.edu>
2021-09-14 15:57:42 -0400

    signin and signup forms and login service

--- Frontend/CapstoneGrocer/src/app/User/sign-in/sign-in.component.ts
+++ Frontend/CapstoneGrocer/src/app/User/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  loginRef = new FormGroup({
+    userid:new FormControl(),
+    password:new FormControl()
+  });
+
+  constructor(public loginSer:LoginService,
+    public router:Router) { }
+    msg?:string
 
   ngOnInit(): void {
+  }
+
+  checkUser() {
+    let login = this.loginRef.value;
+    //console.log(login);
+    this.loginSer.checkLoginDetails(login).
+    subscribe(result=>{
+      if(result=="Success"){
+        // this.router.navigate(["home",login.email]);
+        this.msg = "Sign in successful";
+      }else {
+          this.msg = result;
+      }
+    },
+    error=>console.log(error));
+    this.loginRef.reset();
   }
 
 }
--- Frontend/CapstoneGrocer/src/app/User/sign-up/sign-up.component.ts
+++ Frontend/CapstoneGrocer/src/app/User/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  loginRef = new FormGroup({
+    email:new FormControl(),
+    password:new FormControl(),
+    firstname:new FormControl(),
+    lastname:new FormControl(),
+    phone:new FormControl()
+  })
 
+  constructor(public loginSer:LoginService) { }
+  msg?:string;
   ngOnInit(): void {
+  }
+
+  accountCreate() {
+    let login = this.loginRef.value;
+ 
+    this.loginSer.loginAccountCreate(login).
+    subscribe(result=>this.msg=result,error=>console.log(error));
+ 
+    this.loginRef.reset();
   }
 
 }
