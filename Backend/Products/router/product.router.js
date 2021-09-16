c23a8a71a73355ed6f8a4532a6c045f0dd24485b
Anirudh Reddy Suram <asuram@umassd.edu>
2021-09-16 18:10:18 -0400

    UPDATE

--- Backend/Products/router/product.router.js
+++ Backend/Products/router/product.router.js
@@ -1,11 +1,14 @@
-let express = require('express');
-let router = express.Router(); //router reference
-let ProductController = require("../controller/product.controller.js");
+let express = require("express");
+let router = express.Router();
 
-router.get("/viewProducts",ProductController.getProductDetails);
-router.get("/selectProduct/:pid",ProductController.getProductById);
-router.post("/storeProductDetails",ProductController.storeProdectDetails);
-router.delete("/deleteProductById/:pid",ProductController.deleteProdectById);
-router.put("/updateProductById",ProductController.updateProdectDetails);
+let productController = require("../controller/product.controller");
 
-module.exports=router;
+
+
+router.post("/addProduct", productController.addProduct);
+router.delete("/deleteProduct", productController.deleteProduct);
+router.put("/updateProduct",productController.updateProduct);
+router.get("/getAllProducts",productController.getAll);
+
+
+module.exports = router;
