c701716e8b034b31e1e7a65cfbfb13ad163b7ed5
Bhavankumar Patel <bhavan.patel775@gmail.com>
2021-09-15 22:35:36 -0700

    Backend More User Controller Added/FrontEnd model.tickt.ts added

--- Backend/User/user-model/sales.model.js
+++ Backend/User/user-model/sales.model.js
@@ -3,13 +3,17 @@ let mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 let Saleschema = mongoose.Schema({
-       pid:String,
-       name:String,
-       price:Number,
-       quantity:Number,
-       datePurchased:String,
-       orderStatus:String,
-       userId:String
+       _id:Number,
+       userId:String, //this is userId
+       items: [{
+           pid:String,
+           name:String,
+           price:Number,
+           quantity:Number,
+           datePurchased:String,
+           orderStatus:String,
+       }],
+       total:Number
 })
 let SalesModel = mongoose.model("SoldItems",Saleschema);
 module.exports = SalesModel;
